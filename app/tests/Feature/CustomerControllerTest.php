5d63c19b953d1df7b6144308999f59c757050f5b
Lucas Dauto <lucasdauto@gmail.com>
2023-03-25 17:35:47 -0300

    refactory no teste de cliente

--- app/tests/Feature/CustomerControllerTest.php
+++ app/tests/Feature/CustomerControllerTest.php
@@ -12,6 +12,7 @@ use Tests\TestCase;
 class CustomerControllerTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * A basic feature test example.
      * @test
@@ -20,24 +21,29 @@ class CustomerControllerTest extends TestCase
     {
         $customer = Customer::factory(3)->create();
 
-        $response = $this->getJson('/api/customers');
+        $response = $this->getJson("/api/customers");
 
         $response->assertStatus(200);
         $response->assertJsonCount(3);
         $response->assertJson(function (AssertableJson $json) {
-            $json->whereType('0.id', 'integer');
-            $json->whereType('0.name', 'string');
-            $json->whereType('0.email', 'string');
-            $json->whereType('0.phone', 'string');
-            $json->whereType('0.birthdate', 'string');
-            $json->where('0.birthdate', function($value) {
-                return (new \DateTime($value))->format('Y-m-d') === $value;
-            });
-            $json->whereType('0.address', 'string');
-            $json->whereType('0.complement', 'string|null');
-            $json->whereType('0.neighborhood', 'string');
-            $json->whereType('0.city', 'string');
-            $json->whereType('0.zip_code', 'string');
+            for ($i = 0; $i < 3; $i++) {
+                $json->whereAllType([
+                    "{$i}.id" => "integer",
+                    "{$i}.name" => "string",
+                    "{$i}.email" => "string",
+                    "{$i}.phone" => "string",
+                    "{$i}.birthdate" => "string",
+                    "{$i}.address" => "string",
+                    "{$i}.complement" => "string|null",
+                    "{$i}.neighborhood" => "string",
+                    "{$i}.city" => "string",
+                    "{$i}.zip_code"=> "string"
+                ]);
+
+                $json->where("{$i}.birthdate", function ($value) {
+                    return (new \DateTime($value))->format("Y-m-d") === $value;
+                });
+            }
         });
     }
 }
