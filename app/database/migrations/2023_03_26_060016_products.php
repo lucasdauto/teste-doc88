7ef88507bc8892121ff7a34c4201e4e61b6f66ab
Lucas Dauto <lucasdauto@gmail.com>
2023-03-26 15:08:04 -0300

    migrate de produtos

--- app/database/migrations/2023_03_26_060016_products.php
+++ app/database/migrations/2023_03_26_060016_products.php
@@ -11,7 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price');
+            $table->string('photo');
+            $table->timestamps();
+            $table->softDeletes();
+        });
     }
 
     /**
@@ -19,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('products');
     }
 };
