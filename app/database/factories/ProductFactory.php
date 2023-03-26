a41672e21420dbb3a9ffcca59731241a089d4d99
Lucas Dauto <lucasdauto@gmail.com>
2023-03-26 15:08:46 -0300

    factory de produto

---
+++ app/database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->firstName(),
+            'price' => $this->faker->randomFloat(2, 2, 10),
+            'photo' => $this->faker->imageUrl(),
+        ];
+    }
+}
