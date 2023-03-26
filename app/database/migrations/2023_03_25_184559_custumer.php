496d94c772555f2b66e7dd5014ee38bb761c82e4
Lucas Dauto <lucasdauto@gmail.com>
2023-03-26 14:52:17 -0300

    fix softdelete

--- app/database/migrations/2023_03_25_184559_custumer.php
+++ app/database/migrations/2023_03_25_184559_custumer.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('city');
             $table->string('zip_code');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
