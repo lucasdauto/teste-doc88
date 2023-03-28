b713cb13213c5c8a7429f50b61e455c12fc1aa11
Lucas Dauto <lucasdauto@gmail.com>
2023-03-28 05:30:11 -0300

    cadastro e cancelamento de pedido

--- app/database/seeders/ProductSeeder.php
+++ app/database/seeders/ProductSeeder.php
@@ -3,11 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
 {
+    private $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create();
+    }
+
     /**
      * Run the database seeds.
      */
@@ -17,48 +25,48 @@ class ProductSeeder extends Seeder
         $products = [
             [
                 "name" => "{$pastel} carne",
-                "photo" => $this->fake()->imageUrl(),
+                "photo" => $this->faker->imageUrl(),
                 "price" => 10.00,
             ],
             [
                 "name" => "{$pastel} queijo",
-                "photo" => $this->fake()->imageUrl(),
+                "photo" => $this->faker->imageUrl(),
                 "price" => 10.00,
             ],
             [
                 "name" => "{$pastel} frango",
-                "photo" => $this->fake()->imageUrl(),
+                "photo" => $this->faker->imageUrl(),
                 "price" => 10.00,
             ],
             [
                 "name" => "{$pastel} calabresa",
-                "photo" => $this->fake()->imageUrl(),
+                "photo" => $this->faker->imageUrl(),
                 "price" => 10.00,
             ],
             [
                 "name" => "{$pastel} palmito",
-                "photo" => $this->fake()->imageUrl(),
+                "photo" => $this->faker->imageUrl(),
                 "price" => 10.00,
             ],
             [
                 "name" => "Coca-Cola",
-                "photo" => $this->fake()->imageUrl(),
+                "photo" => $this->faker->imageUrl(),
                 "price" => 5.50,
             ],
             [
                 "name" => "Coca-Cola Zero",
-                "photo" => $this->fake()->imageUrl(),
+                "photo" => $this->faker->imageUrl(),
                 "price" => 5.50,
             ],
             [
                 "name" => "Caldo de cana",
-                "photo" => $this->fake()->imageUrl(),
+                "photo" => $this->faker->imageUrl(),
                 "price" => 5.50,
             ],
         ];
 
         foreach ($products as $product) {
-            Product::create($product);
+            Product::factory()->create($product);
         }
     }
 }
