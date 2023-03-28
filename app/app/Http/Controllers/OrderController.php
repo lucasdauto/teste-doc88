0b733ea39017482097285374546c62f133cd23ff
Lucas Dauto <lucasdauto@gmail.com>
2023-03-28 04:54:19 -0300

    cadastro e cancelamento de pedido

--- app/app/Http/Controllers/OrderController.php
+++ app/app/Http/Controllers/OrderController.php
@@ -26,6 +26,32 @@ class OrderController extends Controller
 
     public function store(OrderRequest $request)
     {
-        return response()->json('foi', 201);
+        $order = $this->order::create([
+            'customer_id' => $request->customer_id,
+        ]);
+
+        foreach ($request->order_items as $item) {
+            $order->orderItems()->create([
+                'product_id' => $item['product_id'],
+                'quantity' => $item['quantity'],
+            ]);
+        }
+
+        return response()->json($order, 201);
+    }
+
+    public function destroy(string $id)
+    {
+        $order = $this->order::find($id);
+
+        if ($order) {
+            $delete = $order->delete();
+            if ($delete) {
+                return response()->json(["message" => "Order cancelled."], 204);
+            }
+            return response()->json(["message" => "Order not cancelled."], 500);
+        }
+
+        return response()->json(["message" => "Order not found."], 404);
     }
 }
